1b15e3ddc5408edc5d444de36179ca3087c11adb
Константин Аввакумов <kcunyaha@gmail.com>
2021-09-14 17:04:14 +0300

    homework from prev module

---
+++ m2_lesson1_step5.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+import time
+import math
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    ans = browser.find_element_by_id("answer")
+    ans.send_keys(y)
+    checkbox = browser.find_element_by_id("robotCheckbox").click()
+    radiobutton = browser.find_element_by_id("robotsRule").click()
+    button = browser.find_element_by_css_selector("button.btn").click()
+    
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
